f5d0be3f06648bc5abdbad4135a660a10d44020d
Diego Finol <dfinol1997@gmail.com>
2024-05-25 00:34:15 +0200

    ver 1

---
+++ src/database.py
@@ -0,0 +1,10 @@
+import mysql.connector 
+
+database: mysql.connector.connect(
+    host = "localhost",
+    user="root",
+    password="Welcome2024!",
+    database="python_crud",
+    port = "3306"
+) 
+
